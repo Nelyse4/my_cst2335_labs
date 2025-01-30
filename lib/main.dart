49a4ac28d6ad22c73b5e3d7d24dc8ed44d6aa5ab
Elyse Niyonagira <niyo0034@algonquinlive.com>
2025-01-30 12:19:45 -0500

    Menu page for different browse categories where its in columns and rows according to their categories

--- lib/main.dart
+++ lib/main.dart
@@ -10,104 +10,151 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Menu Layout',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MenuPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  String imageSource = "images/question-mark.png"; // Default image
-
-  // Function to handle login logic
-  void _login() {
-    final password = _passwordController.text;
-    setState(() {
-      if (password == "QWERTY123") {
-        imageSource = "images/light-bulb.png"; // Correct password: Change to light bulb
-      } else {
-        imageSource = "images/stop.png"; // Incorrect password: Change to stop sign
-      }
-    });
-  }
-
-
+class MenuPage extends StatelessWidget {
+  const MenuPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-          widget.title,
-          style: const TextStyle(color: Colors.black),
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+    Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Text(
+    'BROWSE CATEGORIES',
+    style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold, decoration: TextDecoration.none),
+
+    ),
+    ),
+    Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+    child: Text(
+    'Not sure about exactly which recipe you\'re looking for? Do a search, or dive into our most popular categories.',
+    textAlign: TextAlign.center,
+    style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold, decoration: TextDecoration.none),
+    ),
+    ),
+
+
+        // BY MEAT Section
+        Text(
+          'BY MEAT',
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.none
+          ),
+          textAlign: TextAlign.center,
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Username field
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Login',
-              ),
-            ),
-            const SizedBox(height: 10),
-
-            // Password field
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            const SizedBox(height: 20),
-
-            // Login button
-            ElevatedButton(
-              onPressed: _login,
-              child: const Text(
-                'Login',
-                style: TextStyle(color: Colors.blue, fontSize: 30),
-              ),
-            ),
-            const SizedBox(height: 20),
-
-            // Image that changes based on the password entered
-            Image.asset(
-              imageSource,
-              width: 300,
-              height: 300,
-            ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildImageWithText('BEEF', 'images/beef.jpg'),
+            _buildImageWithText('CHICKEN', 'images/chicken.jpg'),
+            _buildImageWithText('PORK', 'images/pork.jpg'),
+            _buildImageWithText('SEAFOOD', 'images/seafood.jpg'),
           ],
         ),
-      ),
-      // Floating Action Button to clear fields
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add), // Icon for the FAB
-      ),
+
+        // BY COURSE Section
+        Text(
+          'BY COURSE',
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.none
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildImageWithTextBelow('Main Dishes', 'images/main.jpg'),
+            _buildImageWithTextBelow('Salad Recipes', 'images/salad.jpg'),
+            _buildImageWithTextBelow('Side Dishes', 'images/side.jpg'),
+            _buildImageWithTextBelow('Crockpot', 'images/crockpot.jpg'),
+          ],
+        ),
+
+        // BY DESSERT Section
+        Text(
+          'BY DESSERT',
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.none
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildImageWithTextBelow('Ice Cream', 'images/ice_cream.jpg'),
+            _buildImageWithTextBelow('Brownies', 'images/brownies.jpg'),
+            _buildImageWithTextBelow('Pies', 'images/pies.jpg'),
+            _buildImageWithTextBelow('Cookies', 'images/cookies.jpg'),
+          ],
+        ),
+      ],
+    );
+  }
+
+  // Function for image with text in the center
+  Widget _buildImageWithText(String text, String imagePath) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage(imagePath),
+          radius: 50,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.none
+          ),
+        ),
+      ],
+    );
+  }
+
+  // New Function for image with text below
+  Widget _buildImageWithTextBelow(String text, String imagePath) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage(imagePath),
+          radius: 50,
+        ),
+        SizedBox(height: 8), // Space between image and text
+        Text(
+          text,
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.none
+          ),
+        ),
+      ],
     );
   }
 }
+
